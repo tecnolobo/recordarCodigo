4fc224b4c8c0394b692b2af7b36b0b9736bcdbc4
ghtml <jgomez@webcol.net>
2017-12-03 01:35:07 +0100

    Se INSTALO PLUGIN GOOGLE RECAPCHA

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -23,7 +23,7 @@ class ComposerAutoloaderInit2ec5be25b42bec2be41159fdd1d10c4c
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInit2ec5be25b42bec2be41159fdd1d10c4c', 'loadClassLoader'));
 
-        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION');
+        $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
             require_once __DIR__ . '/autoload_static.php';
 
